b9ecd8f69895d30f86d15ccf5b7ccc3383e8df74
Zagidullin-Bulat <120476991+Zagidullin-Bulat@users.noreply.github.com>
2022-12-20 22:48:43 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 1: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Введи число: ");
+int newNumber = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(newNumber);
+
+if (stringNumber.Length < 2)
+{
+    Console.WriteLine("-> второй цифры нет");
+}
+else
+{
+    Console.WriteLine("вторая цифра -> " + stringNumber[1]);
+}
